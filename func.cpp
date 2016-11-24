e4e159192dac2dbe521a64d1f1c333670ed8cca0
Samantha Kacir <skacir@powerpuff.cs.brynmawr.edu>
2016-11-23 19:33:34 -0500

    check all line segments

--- func.cpp
+++ func.cpp
@@ -40,54 +40,57 @@ void returnCrossingIfCrossing(KnotVertex *k, KnotVertex *n){
   
   float slopeToNew = (float)(*(yval)-*(last->getY()))/(*(xval)-*(last->getX()));
   
-  while( k->next != last->next){
-    double firstTest = ((*(k->getX()) - *(last->getX()))*(*yval - *(last->getY()))
-     - (*(k->getY())-*(last->getY()))*(*xval - *(last->getX()))
-     ) * ((*(k->next->getX()) - *(last->getX()))*(*yval - *(last->getY()))
-     - (*(k->next->getY()) - *(last->getY()))*(*xval - *(last->getX()))),
-     secondTest = ((*(last->getX()) - *(k->getX()))*(*(k->next->getY()) - *(k->getY()))
-       - (*yval-*(k->getY()))*(*(k->next->getX()) - *(k->getX()))
-       ) * (
-       (*(last->getX()) - *(k->getX()))*(*(k->next->getY()) - *(k->getY()))
-       -	(*(last->getY()) - *(k->getY()))*(*(k->next->getX()) - *(k->getX())));
+  while( k->next != last){
+    if(k!=n){
+      double firstTest = ((*(k->getX()) - *(last->getX()))*(*yval - *(last->getY()))
+       - (*(k->getY())-*(last->getY()))*(*xval - *(last->getX()))
+       ) * ((*(k->next->getX()) - *(last->getX()))*(*yval - *(last->getY()))
+       - (*(k->next->getY()) - *(last->getY()))*(*xval - *(last->getX()))),
+       secondTest = ((*(last->getX()) - *(k->getX()))*(*(k->next->getY()) - *(k->getY()))
+         - (*yval-*(k->getY()))*(*(k->next->getX()) - *(k->getX()))
+         ) * (
+         (*(last->getX()) - *(k->getX()))*(*(k->next->getY()) - *(k->getY()))
+         -  (*(last->getY()) - *(k->getY()))*(*(k->next->getX()) - *(k->getX())));
 
     #ifdef DEBUG
-       std::cout << "first test: " << signbit(firstTest)<< std::endl;
+         std::cout << "first test: " << signbit(firstTest)<< std::endl;
 
-       std::cout << "second test: " <<  signbit(secondTest) << std:: endl;
+         std::cout << "second test: " <<  signbit(secondTest) << std:: endl;
     #endif
 
-       if ((signbit(firstTest)==1) && (signbit(secondTest) == 1)){
-        KnotVertex *over1, *over2, *under1, *under2;
-        if(k->getSlopeToNext() < slopeToNew){
-	       //new line under
-         over1 = k;
-         over2 = k->next;
-         under1 = last;
-         under2 = n;
-       }
-       else{	  
-	       //new line over
-         under1 = k;
-         under2 =k->next;
-         over1 = last;
-         over2 = n;
-       }
+         if ((signbit(firstTest)==1) && (signbit(secondTest) == 1)){
+          KnotVertex *over1, *over2, *under1, *under2;
+          if(k->getSlopeToNext() < slopeToNew){
+         //new line under
+           over1 = k;
+           over2 = k->next;
+           under1 = last;
+           under2 = n;
+         }
+         else{    
+         //new line over
+           under1 = k;
+           under2 =k->next;
+           over1 = last;
+           over2 = n;
+         }
 
       #ifdef DEBUG
-       knotNot knt = knotNot(k, k->next, last, n);
-       knotNot knt2 = knotNot(k, k->next, n, last); 
-       knotNot knt3 = knotNot(k->next, k, last, n);
-       knotNot knt4 = knotNot(k->next, k, n, last); 
-       knt.printNot();
-       knt2.printNot();
-       knt3.printNot();
-       knt4.printNot();
+         knotNot knt = knotNot(k, k->next, last, n);
+         knotNot knt2 = knotNot(k, k->next, n, last); 
+         knotNot knt3 = knotNot(k->next, k, last, n);
+         knotNot knt4 = knotNot(k->next, k, n, last); 
+         knt.printNot();
+         knt2.printNot();
+         knt3.printNot();
+         knt4.printNot();
       #endif
 
-       k->insert(knotNot(over1, over2, under1, under2));
-       last->insert(knotNot(over1, over2, under1, under2));
+         k->insert(knotNot(over1, over2, under1, under2));
+         last->insert(knotNot(over1, over2, under1, under2));
+       }
      }
+
 
      k = k->next;
    }
@@ -124,8 +127,10 @@ void generateKnot(KnotVertex* k, int n) {
     xvals[i] = tempx;
     yvals[i] = tempy;
 
-    k->add(new KnotVertex(xvals + i, yvals + i));    
+    k->add(new KnotVertex(xvals + i, yvals + i));   
+    //k->printAll(); 
   }
+  returnCrossingIfCrossing(k, k);
 
   k->printAll();
 }
