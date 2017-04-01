d7440840ccb26b776889fe42276596f3e5a9ffb8
samphibian <root@debianKnot>
2017-03-31 20:25:50 -0400

    added copyright notice

--- knotNot.cpp
+++ knotNot.cpp
@@ -1,4 +1,23 @@
-//given a knot, should output notation. notation should have sign, crossing id, point of intersection, and the KnotVertex pointers for a, b, c, d
+/*
+ * Copyright (C) 2017  Samantha Kacir
+ *
+ * given a knot, should output notation. notation should have sign, crossing id, point of intersection, and the KnotVertex pointers for a, b, c, d
+ * 
+ * This file is part of knot-generation-and-analysis.
+ * 
+ * knot-generation-and-analysis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * knot-generation-and-analysis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with knot-generation-and-analysis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "knot.h"
 #include <string>
--- knots.cpp
+++ knots.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017  Samantha Kacir
+ * 
+ * This file is part of knot-generation-and-analysis.
+ * 
+ * knot-generation-and-analysis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * knot-generation-and-analysis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with knot-generation-and-analysis.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <math.h>
 #include <sstream> //for stringstream
@@ -46,6 +64,8 @@ int main(){
 	srand(time(NULL));
   KnotVertex * knot;
 
+  std::cout << "\nknot-generation-and-analysis Copyright (C) 2017 Samantha Kacir\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under certain conditions."
+    << std::endl << std::endl;
   int n;
   std::string outputFileBaseName = "generatedKnots", outputFileExt = ".txt";
 
